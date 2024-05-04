570fcc8f0b7c18ee679a216e21e49923ab8e30ae
Diego Barrales Castillo <d.castillo@nexnox.com>
2024-05-04 15:19:12 +0200

    Revert "Update all dependencies and removed the ones that are replaced by latest node features."
    This reverts commit eb28d609e9a130460fdc3c0400cf834fb1297a16.

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // URI para conectar al cluster
 const uri = process.env.DB_URI || "mongodb://127.0.0.1:27017/TodoApp";
 
 // Opciones para el cliente de MongoDB
 const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
   dbName: "NoteThat"
 };
 
